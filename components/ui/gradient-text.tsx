dcb8ab41a3165454e1a2199e6972a432989e4b3e
Erudite098 <macalde.kerzania1029@gmail.com>
2025-07-07 23:28:33 +0800

    issue: linear gradient stretching

--- components/ui/gradient-text.tsx
+++ components/ui/gradient-text.tsx
@@ -1,7 +1,6 @@
-import MaskedView from '@react-native-masked-view/masked-view';
 import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
-import { StyleSheet, Text, TextProps, View } from 'react-native';
+import { StyleSheet, Text, TextProps } from 'react-native';
 
 interface GradientTextProps extends TextProps {
   text: string;
@@ -23,50 +22,42 @@ export default function GradientText({
   };
 
   return (
-    <View style={styles.wrapper}>
-      <MaskedView
-        style={styles.maskedContainer}
-        maskElement={
-          <Text
-            {...props}
-            style={[
-              styles.maskedText,
-              style,
-              {
-                fontSize,
-                fontFamily: fontMap[fontWeight],
-              },
-            ]}
-          >
-            {text}
-          </Text>
-        }
+    // <MaskedView
+    //   maskElement={
+    //     <Text
+    //       {...props}
+    //       style={[
+    //         styles.baseText,
+    //         { fontSize, fontFamily: fontMap[fontWeight], color: 'black' },
+    //         style,
+    //       ]}
+    //     >
+    //       {text}
+    //     </Text>
+    //   }
+    // >
+      <LinearGradient
+        colors={['#D8125A', '#E88AB0', '#CB6190', '#D8125A']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
       >
-        <LinearGradient
-          colors={['#D8125A', '#E88AB0', '#CB6190', '#D8125A']}
-          locations={[0, 0.46, 0.64, 0.95]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 5 }}
-          style={styles.gradientFill}
+        <Text
+          {...props}
+          style={[
+            styles.baseText,
+            { fontSize, fontFamily: fontMap[fontWeight], opacity: 0 },
+            style,
+          ]}
         >
-          <Text style={[style, { opacity: 0 }]}>{text}</Text>
-        </LinearGradient>
-      </MaskedView>
-    </View>
+          {text}
+        </Text>
+      </LinearGradient>
+    // </MaskedView>
   );
 }
 
 const styles = StyleSheet.create({
-  wrapper: { 
-    minHeight: 29, // avoids clipping tall characters
-  },
-  maskedContainer: {
-    flex: 1,
-  },
-  gradientFill: {
-    flex: 1,
-  },
-  maskedText: {
+  baseText: {
     backgroundColor: 'transparent',
   },
 });
