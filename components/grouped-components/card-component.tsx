a4b1078f02aa5b215865ab29418ab9b0a2ca0af9
Erudite098 <macalde.kerzania1029@gmail.com>
2025-07-27 13:52:34 +0800

    combined gradient-card, grad-text, grad-btn

---
+++ components/grouped-components/card-component.tsx
@@ -0,0 +1,88 @@
+// TODO: Combine gradient-card, gradient-button, gradient-text all in one
+// with parameters for title, subtitle, and content
+import GradientButton from '@/components/individual-component/gradient-button';
+import GradientCard from '@/components/individual-component/gradient-card';
+import GradientText from '@/components/individual-component/gradient-text';
+import { ChevronRight } from 'lucide-react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default function CardComponent() {
+    return (
+        <View>
+            <Image source={require('@/assets/images/pig.png')} style={styles.pigImage} />
+           
+            {/* Card will sit within the 23px padded area */}
+            <GradientCard >
+                <View>
+                <GradientText 
+                text="₱ 1,250.00" 
+                fontSize={22} 
+                fontWeight="Regular" 
+                />
+                
+                <Text style={{ 
+                    marginBottom: 7, 
+                    color: '#CC5B8F', 
+                    fontSize: 12 }}>
+                    Amount Colected
+                </Text>
+
+                <GradientText 
+                    text="₱ 2000.00" 
+                    fontSize={22} 
+                    fontWeight="Regular"  
+                />
+
+                <Text style={{ 
+                    marginBottom: 10, 
+                    color: '#CC5B8F', 
+                    fontSize: 12 }}>
+                    Total Due
+                </Text>
+
+                <GradientText 
+                    text="Summer Course" 
+                    fontSize={22} 
+                    fontWeight="Bold"
+                    style={{ marginBottom: 10}}
+                />
+
+                <GradientText 
+                    text="Paymnent for intensive courses" 
+                    fontSize={14} 
+                    fontWeight="Regular"
+                
+                />
+
+                <GradientButton 
+                    onPress={() => {}}
+                    icon={<ChevronRight size={16} color="white" />}
+                >
+                    <Text style={{ 
+                    fontSize: 12,
+                    color: 'white', 
+                    textAlign: 'center'}}>
+                    View details
+                    </Text>
+                </GradientButton>
+                </View>
+                    
+            </GradientCard>
+                    
+    
+
+        </View>
+    );
+}   
+
+const styles = StyleSheet.create({
+  pigImage: {
+    width: 150,
+    height: 150,
+    position: 'absolute',
+    right: 35,
+    top: 0,
+    zIndex: 1,
+  }
+});
---
+++ components/individual-component/gradient-card.tsx
@@ -0,0 +1,55 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+interface GradientCardProps {
+  children?: React.ReactNode; // The content of the card, can be a string, number, element or an array of elements.
+  borderRadius?: number;
+}
+
+export default function GradientCard({
+  children, 
+  borderRadius = 16, 
+  
+}: GradientCardProps) { 
+  return (
+    <LinearGradient 
+      colors={['#F4BBD0', '#F2EAED']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={[styles.card, { borderRadius }]}
+    > 
+      {/* The card is styled with a gradient background and a border radius */}
+      <View style={styles.content}>  
+        {children}
+      </View>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    width: '100%',
+    padding: 20, // Padding inside the card
+    marginBottom: 23, // Margin at the bottom to separate from other elements
+    flexShrink: 1, // allow the height to grow
+    // alignSelf: 'stretch', // important for full width in flex parents
+    
+    // iOS Shadow
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+
+    // Android Shadow, need elavation
+    elevation: 3,
+
+    // Background and borderRadius (kasi needed para makita 'yung shadow)
+    backgroundColor: '#fff',
+    borderRadius: 12,
+  },
+  content: {
+    justifyContent: 'flex-start',
+    flexShrink: 1, // allow inner content to grow
+  },
+});
